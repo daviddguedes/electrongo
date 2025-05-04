a8a38f1559d1168e593cca985850673d53590268
David Coelho <guedes0379@gmail.com>
2025-05-04 12:07:30 +0100

    update home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,45 +5,6 @@ import MonacoEditor from 'react-monaco-editor';
 
 const mongoURI = 'mongodb://0.0.0.0:27017/quote';
 
-const MUI_X_PRODUCTS = [
-    {
-        "id": 'grid',
-        "label": 'Data Grid',
-        "children": [
-            {
-                "id": 'grid-community', "label": '@mui/x-data-grid', "children": [
-                    {
-                        "id": 'pickers',
-                        "label": 'Date and Time Pickers',
-                        "children": [
-                            { "id": 'pickers-community', "label": '@mui/x-date-pickers' },
-                            { "id": 'pickers-pro', "label": '@mui/x-date-pickers-pro' },
-                        ],
-                    },
-                ]
-            },
-            { "id": 'grid-pro', "label": '@mui/x-data-grid-pro' },
-            { "id": 'grid-premium', "label": '@mui/x-data-grid-premium' },
-        ],
-    },
-    {
-        "id": 'charts',
-        "label": 'Charts',
-        "children": [
-            { "id": 'charts-community', "label": '@mui/x-charts' },
-            { "id": 'charts-pro', "label": '@mui/charts-pro' },
-        ],
-    },
-    {
-        "id": 'tree-view',
-        "label": 'Tree View',
-        "children": [
-            { "id": 'tree-view-community', "label": '@mui/x-tree-view' },
-            { "id": 'tree-view-pro', "label": '@mui/x-tree-view-pro' },
-        ],
-    },
-];
-
 const recurssiveTransform = (item) => {
     const nodes = [];
     Object.keys(item).forEach(key => {
@@ -82,12 +43,7 @@ const HomePage = () => {
     const editorRef = useRef(null);
     const [documents, setDocuments] = useState([]);
 
-    const initialQuery = `async function fetchMessages() {
-    const messages = await Message.find();
-    console.log("ok");
-}
-
-fetchMessages();`;
+    const initialQuery = ``;
 
     const handleEditorDidMount = (editor) => {
         editorRef.current = editor;
